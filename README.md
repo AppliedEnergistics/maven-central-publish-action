5c1548cf91e79ce24fed57a634c4f369f75242ef
Sebastian Hartte <shartte@users.noreply.github.com>
2025-01-27 20:59:28 +0100

    Package

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Takes a local Maven repository (i.e. created via Gradle publish to a file URL)
 and pushes all contained artifacts to Maven central.
 
-Uses the publisher API:
-https://central.sonatype.org/publish/publish-portal-api/
+Uses the publisher API: https://central.sonatype.org/publish/publish-portal-api/
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,74 @@
 import * as core from '@actions/core'
 import * as os from 'os'
 import * as path from 'path'
-import * as fs from "node:fs";
-import {create} from 'tar'
+import * as fs from 'node:fs'
+import { create } from 'tar'
 
 async function main(): Promise<void> {
-    /**
-     * The main function for the action.
-     * @returns {Promise<void>} Resolves when the action is complete.
-     */
-    try {
-        const localPath: string = core.getInput('local-repository-path')
-        const apiUrl: string = core.getInput('api-url')
-        const deploymentName: string = core.getInput('deployment-name');
-        const manualPublishing: boolean = core.getBooleanInput('manual-publishing');
-        const remoteUsername: string = core.getInput('remote-repository-username')
-        const remotePassword: string = core.getInput('remote-repository-password')
-        let tempDir: string = core.getInput('temp-dir')
-        if (!tempDir) {
-            tempDir = os.tmpdir()
-        }
-        core.setSecret(remotePassword)
-
-        const bundlePath = path.join(tempDir, 'bundle.tar.gz');
-
-        await create(
-            {
-                gzip: true,
-                file: bundlePath,
-                cwd: localPath
-            },
-            ["."]
-        );
-
-        const bundleBlob = new Blob([fs.readFileSync(bundlePath)]);
-
-        const formData = new FormData();
-        formData.append('bundle', bundleBlob);
-        if (deploymentName) {
-            formData.append("name", deploymentName);
-        }
-        if (manualPublishing) {
-            formData.append("publishingType", "USER_MANAGED");
-        }
-        const token = atob(`${remoteUsername}:${remotePassword}`);
-        core.setSecret(token);
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-        let responseText: string = '';
-        try {
-            responseText = await response.text();
-        } catch {
-        }
-
-        if (!response.ok) {
-            core.setFailed(`Failed to upload bundle to ${apiUrl}: Status ${response.status}\n${responseText}`);
-        } else {
-            core.setOutput("deployment-id", responseText.trim());
-        }
-    } catch (error) {
-        // Fail the workflow run if an error occurs
-        if (error instanceof Error) core.setFailed(error.message)
+  /**
+   * The main function for the action.
+   * @returns {Promise<void>} Resolves when the action is complete.
+   */
+  try {
+    const localPath: string = core.getInput('local-repository-path')
+    const apiUrl: string = core.getInput('api-url')
+    const deploymentName: string = core.getInput('deployment-name')
+    const manualPublishing: boolean = core.getBooleanInput('manual-publishing')
+    const remoteUsername: string = core.getInput('remote-repository-username')
+    const remotePassword: string = core.getInput('remote-repository-password')
+    let tempDir: string = core.getInput('temp-dir')
+    if (!tempDir) {
+      tempDir = os.tmpdir()
     }
+    core.setSecret(remotePassword)
+
+    const bundlePath = path.join(tempDir, 'bundle.tar.gz')
+
+    await create(
+      {
+        gzip: true,
+        file: bundlePath,
+        cwd: localPath
+      },
+      ['.']
+    )
+
+    const bundleBlob = new Blob([fs.readFileSync(bundlePath)])
+
+    const formData = new FormData()
+    formData.append('bundle', bundleBlob)
+    if (deploymentName) {
+      formData.append('name', deploymentName)
+    }
+    if (manualPublishing) {
+      formData.append('publishingType', 'USER_MANAGED')
+    }
+    const token = atob(`${remoteUsername}:${remotePassword}`)
+    core.setSecret(token)
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    let responseText: string = ''
+    try {
+      responseText = await response.text()
+    } catch {}
+
+    if (!response.ok) {
+      core.setFailed(
+        `Failed to upload bundle to ${apiUrl}: Status ${response.status}\n${responseText}`
+      )
+    } else {
+      core.setOutput('deployment-id', responseText.trim())
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
 
 // noinspection JSIgnoredPromiseFromCall
